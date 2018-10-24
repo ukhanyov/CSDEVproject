a138741213a082f597f34b1300e3012767b2f24f
admin-linux <ukhanyov.oleg@gmail.com>
2018-10-24 12:55:54 +0300

    added FABs

--- app/src/main/java/com/example/admin_linux/csdevproject/MainActivity.java
+++ app/src/main/java/com/example/admin_linux/csdevproject/MainActivity.java
@@ -1,52 +1,113 @@
 package com.example.admin_linux.csdevproject;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.admin_linux.csdevproject.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // Fancy dataBinding
+    ActivityMainBinding mBinding;
+
+    private Boolean isFabOpen = false;
+    private FloatingActionButton fab, fab1, fab2, fab3, fab4;
+    private Animation fab_open,fab_close,rotate_forward,rotate_backward;
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        // Toolbar
+        Toolbar toolbar = mBinding.toolbar;
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // FAB init
+        fab = mBinding.fab;
+        fab1 = mBinding.fab1;
+        fab2 = mBinding.fab2;
+        fab3 = mBinding.fab3;
+        fab4 = mBinding.fab4;
+        // Animations init
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
+        rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_forward);
+        rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_backward);
+        // Click listeners for FAB
+        fab.setOnClickListener(this);
+        fab1.setOnClickListener(this);
+        fab2.setOnClickListener(this);
+        fab3.setOnClickListener(this);
+        fab4.setOnClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.fab:
+                animateFAB();
+                break;
+            case R.id.fab1:
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+                Log.d(LOG_TAG, "Fab 1");
+                break;
+            case R.id.fab2:
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                Log.d(LOG_TAG, "Fab 2");
+                break;
+            case R.id.fab3:
+
+                Log.d(LOG_TAG, "Fab 3");
+                break;
+            case R.id.fab4:
+
+                Log.d(LOG_TAG, "Fab 4");
+                break;
         }
 
-        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void animateFAB(){
+
+        if(isFabOpen){
+            fab.startAnimation(rotate_backward);
+            fab1.startAnimation(fab_close);
+            fab2.startAnimation(fab_close);
+            fab3.startAnimation(fab_close);
+            fab4.startAnimation(fab_close);
+
+            fab1.setClickable(false);
+            fab2.setClickable(false);
+            fab3.setClickable(false);
+            fab4.setClickable(false);
+            isFabOpen = false;
+            Log.d(LOG_TAG, "close");
+        } else {
+            fab.startAnimation(rotate_forward);
+            fab1.startAnimation(fab_open);
+            fab2.startAnimation(fab_open);
+            fab3.startAnimation(fab_open);
+            fab4.startAnimation(fab_open);
+
+            fab1.setClickable(true);
+            fab2.setClickable(true);
+            fab3.setClickable(true);
+            fab4.setClickable(true);
+            isFabOpen = true;
+            Log.d(LOG_TAG,"open");
+        }
     }
 }
