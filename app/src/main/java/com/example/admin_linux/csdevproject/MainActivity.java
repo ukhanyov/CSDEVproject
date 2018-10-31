5efddd8eb115829ae21cc27c838f095e1ee5d721
admin-linux <ukhanyov.oleg@gmail.com>
2018-10-31 10:22:18 +0200

    overall code cleanup, FAB: margins, animation time, slight refactoring

--- app/src/main/java/com/example/admin_linux/csdevproject/MainActivity.java
+++ app/src/main/java/com/example/admin_linux/csdevproject/MainActivity.java
@@ -7,11 +7,8 @@ import android.os.Bundle;
 import android.os.Parcelable;
 import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
-import android.support.design.bottomnavigation.LabelVisibilityMode;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -51,16 +48,16 @@ public class MainActivity extends AppCompatActivity implements
     ActivityMainBinding mBinding;
 
     private Boolean isFabOpen = false;
-    private FloatingActionButton fab, fab1, fab2, fab3, fab4, fab5, fab6, fab7, fab8;
-    private Animation fab_open_1, fab_open_2, fab_open_3, fab_open_4, fab_open_5, fab_open_6, fab_open_7, fab_open_8,
-            fab_close_1, fab_close_2, fab_close_3, fab_close_4, fab_close_5, fab_close_6, fab_close_7, fab_close_8,
+    private Animation fab_title_open_1, fab_title_open_2, fab_title_open_3, fab_title_open_4,
+            fab_open_1, fab_open_2, fab_open_3, fab_open_4,
+            fab_title_close_1, fab_title_close_2, fab_title_close_3, fab_title_close_4,
+            fab_close_1, fab_close_2, fab_close_3, fab_close_4,
             rotate_forward, rotate_backward;
 
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    //private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     // Fragment stuff
     FragmentManager fragmentManager;
-    FragmentTransaction fragmentTransaction;
 
     BottomNavigationView bottomNavigationView;
 
@@ -97,44 +94,11 @@ public class MainActivity extends AppCompatActivity implements
         bottomNavigationView.setItemIconTintList(null);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
 
-        // FAB init
-        fab = mBinding.fab;
-        fab1 = mBinding.fab1;
-        fab2 = mBinding.fab2;
-        fab3 = mBinding.fab3;
-        fab4 = mBinding.fab4;
-        fab5 = mBinding.fab5;
-        fab6 = mBinding.fab6;
-        fab7 = mBinding.fab7;
-        fab8 = mBinding.fab8;
-
         // Animations init
-        fab_open_1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open_1);
-        fab_open_2 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open_2);
-        fab_open_3 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open_3);
-        fab_open_4 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open_4);
-        fab_open_5 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open_5);
-        fab_open_6 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open_6);
-        fab_open_7 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open_7);
-        fab_open_8 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open_8);
+        initAnimations();
 
-        fab_close_1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close_1);
-        fab_close_2 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close_2);
-        fab_close_3 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close_3);
-        fab_close_4 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close_4);
-        fab_close_5 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close_5);
-        fab_close_6 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close_6);
-        fab_close_7 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close_7);
-        fab_close_8 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close_8);
-
-        rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_forward);
-        rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_backward);
         // Click listeners for FAB
-        fab.setOnClickListener(this);
-        fab1.setOnClickListener(this);
-        fab2.setOnClickListener(this);
-        fab3.setOnClickListener(this);
-        fab4.setOnClickListener(this);
+        mBinding.fab.setOnClickListener(this);
 
         // Start activity with CropStreamFragment
         fragmentManager = getSupportFragmentManager();
@@ -168,90 +132,8 @@ public class MainActivity extends AppCompatActivity implements
             case R.id.fab:
                 animateFAB();
                 break;
-            case R.id.fab1:
-
-                Log.d(LOG_TAG, "Fab 1");
-                break;
-            case R.id.fab2:
-
-                Log.d(LOG_TAG, "Fab 2");
-                break;
-            case R.id.fab3:
-
-                Log.d(LOG_TAG, "Fab 3");
-                break;
-            case R.id.fab4:
-
-                Log.d(LOG_TAG, "Fab 4");
-                break;
         }
 
-    }
-
-    public void animateFAB() {
-
-        if (isFabOpen) {
-
-            fab.startAnimation(rotate_backward);
-            fab1.startAnimation(fab_close_4);
-            fab2.startAnimation(fab_close_3);
-            fab3.startAnimation(fab_close_2);
-            fab4.startAnimation(fab_close_1);
-            fab5.startAnimation(fab_close_5);
-            fab6.startAnimation(fab_close_6);
-            fab7.startAnimation(fab_close_7);
-            fab8.startAnimation(fab_close_8);
-
-            fab1.setClickable(false);
-            fab2.setClickable(false);
-            fab3.setClickable(false);
-            fab4.setClickable(false);
-            fab5.setClickable(false);
-            fab6.setClickable(false);
-            fab7.setClickable(false);
-            fab8.setClickable(false);
-            isFabOpen = false;
-
-            mBinding.tvFavTitle1.startAnimation(fab_close_4);
-            mBinding.tvFavTitle2.startAnimation(fab_close_3);
-            mBinding.tvFavTitle3.startAnimation(fab_close_2);
-            mBinding.tvFavTitle4.startAnimation(fab_close_1);
-
-            mBinding.tvFavTitle1.setVisibility(View.INVISIBLE);
-            mBinding.tvFavTitle2.setVisibility(View.INVISIBLE);
-            mBinding.tvFavTitle3.setVisibility(View.INVISIBLE);
-            mBinding.tvFavTitle4.setVisibility(View.INVISIBLE);
-        } else {
-            fab.startAnimation(rotate_forward);
-            fab1.startAnimation(fab_open_1);
-            fab2.startAnimation(fab_open_2);
-            fab3.startAnimation(fab_open_3);
-            fab4.startAnimation(fab_open_4);
-            fab5.startAnimation(fab_open_5);
-            fab6.startAnimation(fab_open_6);
-            fab7.startAnimation(fab_open_7);
-            fab8.startAnimation(fab_open_8);
-
-            fab1.setClickable(true);
-            fab2.setClickable(true);
-            fab3.setClickable(true);
-            fab4.setClickable(true);
-            fab5.setClickable(true);
-            fab6.setClickable(true);
-            fab7.setClickable(true);
-            fab8.setClickable(true);
-            isFabOpen = true;
-
-            mBinding.tvFavTitle1.startAnimation(fab_open_1);
-            mBinding.tvFavTitle2.startAnimation(fab_open_2);
-            mBinding.tvFavTitle3.startAnimation(fab_open_3);
-            mBinding.tvFavTitle4.startAnimation(fab_open_4);
-
-            mBinding.tvFavTitle1.setVisibility(View.VISIBLE);
-            mBinding.tvFavTitle2.setVisibility(View.VISIBLE);
-            mBinding.tvFavTitle3.setVisibility(View.VISIBLE);
-            mBinding.tvFavTitle4.setVisibility(View.VISIBLE);
-        }
     }
 
     @Override
@@ -310,6 +192,79 @@ public class MainActivity extends AppCompatActivity implements
     public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
         super.onSaveInstanceState(outState, outPersistentState);
         getSupportFragmentManager().putFragment(outState, "CropStreamFragment", fragmentCropStreamTransaction);
+    }
+
+    private void initAnimations(){
+        fab_title_open_1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_title_open_1);
+        fab_title_open_2 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_title_open_2);
+        fab_title_open_3 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_title_open_3);
+        fab_title_open_4 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_title_open_4);
+        fab_open_1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open_1);
+        fab_open_2 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open_2);
+        fab_open_3 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open_3);
+        fab_open_4 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open_4);
+
+        fab_title_close_1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_title_close_1);
+        fab_title_close_2 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_title_close_2);
+        fab_title_close_3 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_title_close_3);
+        fab_title_close_4 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_title_close_4);
+        fab_close_1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close_1);
+        fab_close_2 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close_2);
+        fab_close_3 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close_3);
+        fab_close_4 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close_4);
+
+        rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_forward);
+        rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_backward);
+    }
+
+    private void animateFAB() {
+
+        if (isFabOpen) {
+
+            mBinding.fab.startAnimation(rotate_backward);
+            mBinding.fab1.startAnimation(fab_close_1);
+            mBinding.fab2.startAnimation(fab_close_2);
+            mBinding.fab3.startAnimation(fab_close_3);
+            mBinding.fab4.startAnimation(fab_close_4);
+
+            mBinding.fab1.setClickable(false);
+            mBinding.fab2.setClickable(false);
+            mBinding.fab3.setClickable(false);
+            mBinding.fab4.setClickable(false);
+            isFabOpen = false;
+
+            mBinding.tvFavTitle1.startAnimation(fab_title_close_4);
+            mBinding.tvFavTitle2.startAnimation(fab_title_close_3);
+            mBinding.tvFavTitle3.startAnimation(fab_title_close_2);
+            mBinding.tvFavTitle4.startAnimation(fab_title_close_1);
+
+            mBinding.tvFavTitle1.setVisibility(View.INVISIBLE);
+            mBinding.tvFavTitle2.setVisibility(View.INVISIBLE);
+            mBinding.tvFavTitle3.setVisibility(View.INVISIBLE);
+            mBinding.tvFavTitle4.setVisibility(View.INVISIBLE);
+        } else {
+            mBinding.fab.startAnimation(rotate_forward);
+            mBinding.fab1.startAnimation(fab_open_1);
+            mBinding.fab2.startAnimation(fab_open_2);
+            mBinding.fab3.startAnimation(fab_open_3);
+            mBinding.fab4.startAnimation(fab_open_4);
+
+            mBinding.fab1.setClickable(true);
+            mBinding.fab2.setClickable(true);
+            mBinding.fab3.setClickable(true);
+            mBinding.fab4.setClickable(true);
+            isFabOpen = true;
+
+            mBinding.tvFavTitle1.startAnimation(fab_title_open_1);
+            mBinding.tvFavTitle2.startAnimation(fab_title_open_2);
+            mBinding.tvFavTitle3.startAnimation(fab_title_open_3);
+            mBinding.tvFavTitle4.startAnimation(fab_title_open_4);
+
+            mBinding.tvFavTitle1.setVisibility(View.VISIBLE);
+            mBinding.tvFavTitle2.setVisibility(View.VISIBLE);
+            mBinding.tvFavTitle3.setVisibility(View.VISIBLE);
+            mBinding.tvFavTitle4.setVisibility(View.VISIBLE);
+        }
     }
 
     private void fetchData() {
